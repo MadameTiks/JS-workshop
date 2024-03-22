e8b23a2b2ffea23fd3e7337e50abef0c2521c5d0
Tika <162357343+MadameTiks@users.noreply.github.com>
2024-03-22 11:18:35 -0400

    1st Commit

---
+++ ONE.js
@@ -0,0 +1,99 @@
+
+
+
+let fruits = ["apple", "pear", "banana", "orange", "cherry", "bluberry", "kiwi", "strawbery", "peach", "tangerine", "fig", "aiva", "pomegrant"];
+
+console.log(fruits.length);
+
+fruits.push("sunflower")
+
+console.log(fruits);
+
+
+let rooms= ["bedroom", "livingroom", "basment"];
+
+rooms.push("kitchen")
+
+  console.log(rooms)
+
+
+  let colors= ["green", "black", "yellow", "red"];
+
+  colors.push("ginger")
+
+  console.log(colors);
+
+
+
+  const numbers= [11, 32, 43, 34, 654,];
+
+  numbers.push(9999)
+
+  console.log(numbers);
+
+
+  let flowers= ["lotus", "lilac", "canabis"];
+   
+   flowers.unshift("bluebell")
+
+   console.log(flowers);
+
+   let years = [1992, 2024, 1950];
+   
+   years.unshift(1969)
+
+   console.log(years);
+
+
+   let animals=["awl", "racoon", "fox", "fish"];
+
+   animals.shift()
+
+   console.log(animals);
+
+   let cars=["audi", "nisan", "jeep", "tesla"];
+
+   cars.shift()
+
+   console.log(cars);
+
+   cars.unshift("tesla")
+
+   console.log(cars);
+
+   cars.push("volga")
+
+   console.log(cars);
+
+
+let animal = [ "fox", "wolf", "eliphant"];
+let bird = ["eagle", "awl",];
+console.log(animal.concat(bird));
+
+
+let pets = ["cat", "dog", "squirrel"];
+
+for (i=0; i<=2; i++);
+console.log(pets);
+
+
+
+let b= 7;
+
+for (b=7; b<58; b+=5) {
+
+    console.log(b)
+    
+}
+
+
+
+let friend = ["ana", "tika", "natia"];
+
+let a=0; 
+
+for (a; a<3; a++) {
+    console.log(friend[a])
+}
+
+
